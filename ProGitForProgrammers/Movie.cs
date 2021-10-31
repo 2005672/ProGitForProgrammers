33306852a5ca42ba93d2ff816fcd789f87f4543d
Ross Hamilton <2005672@uad.ac.uk>
2021-10-31 14:42:31 +0000

    Create Movie.cs

---
+++ ProGitForProgrammers/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    class Movie
+    {
+        public string Title { get; set; }
+
+        public List<string> Actors { get; set; }
+    }
+}
